83f64d4ccb0ed7d1278d143f8c0c0b79dd593ceb
Kat9_123 <56130489+Kat9-123@users.noreply.github.com>
2021-03-02 15:47:42 +0100

    Update HashRotation.py

--- HashRotation.py
+++ HashRotation.py
@@ -1,7 +1,11 @@
 # Made by: Kat9_123
+# This is a simple encryption algorithm that is based on the caesar cipher
+# it has some randomly generated "salt" at the beginning of the message
+# to try to circumvent a KPA
 from hashlib import sha256
 from binascii import b2a_base64
-
+from os import urandom
+import base64
 
 
 
@@ -9,52 +13,72 @@ from binascii import b2a_base64
 characterSet = "abcdefghijklmnopqrstuvwxyz"
 characterSet += characterSet.upper()
 characterSet += "0123456789"
-characterSet += ". "
-characterSet *= 2
+characterSet += "/+"
+characterSet *= 8
 
 type = False # ENCRYPT
 #type = True  # DECRYPT
 
+
+
+
+
+# Get user input
 key = input("Key: ")
 
-print("[E] Encrypt or [D] Decrypt")
-ch = input(">").upper()
-if ch == "D":
-	type = True
+print("Enter text")
+inputText = input("").replace(".", "/").replace(" ", "+")
 
-print("Enter", "ciphertext" if type else "plaintext")
-inputText = input("")
+
+print("[E]ncrypt or [D]ecrypt")
+ch = input(">").upper()
 
 out = ""
 
+# Handle decryption AND "salt" to circumvent a KPA
+if ch == "D":
+	type = True
+	salt = inputText.split("=")[0] + "="
+	inputText = inputText.split("=")[1]
+else:
+	salt = base64.b64encode(urandom(32)).decode()
+	out = extra
+
+key = salt + key
+
+
+
+
+
 keyAddition = ""
-loops = -(-len(inputText) // 16) # CEIL
+loops = -(-len(inputText) // 32) # CEIL
+
+# Loop through 32 character blocks
 for z in range(loops):
 	indexArray = []
 
 	# Generate hash
 	hash = sha256((keyAddition + key).encode()).hexdigest()
 
-	# Generate offset based on hash
-	for i in range(len(hash)//4):
-		indexArray.append(-1)
-		for x in range(4):
-			indexArray[i] += int(hash[i*4+x], 16) + 1
+	# Generate 32 offsets based on hash
+	for i in range(len(hash)//2):
+		indexArray.append(int(hash[(i*2):(i*2+2)], 16))
 
-	# If block mod 16 isnt 0 this part helps
-	characters = 16
+	# Last block
+	characters = 32
 	if z == loops - 1:
-		characters = len(inputText) % 16
+		characters = len(inputText) % 32
 
 	# Rotate
 	for i in range(characters):
-		#print(i)
 		if type:
 			indexArray[i] = -indexArray[i]
 
-		out += characterSet[characterSet.find(inputText[i+z*16])+indexArray[i]]
+		char = inputText[i+z*32]
+		index = characterSet.find(char)
+		out += characterSet[index+indexArray[i]]
 	
 	keyAddition = hash	
 
-print("Plaintext:" if type else "Ciphertext:",  out)
+print(("Plaintext: " + out.replace("+"," ").replace("/",".")) if type else ("Ciphertext: " + out))
 input()
